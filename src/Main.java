885e1903f17ab847700c001f718488a8cb46b4fe
jiangTaoQuite <jiangtao103cp@163.com>
2016-08-27 13:03:03 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Class: Main </br>
+ * Description:  入口程序</br>
+ * Creator: Kevin </br>
+ * Date: 2016/8/25 1:58 </br>
+ * Update: 2016/8/25 2016/8/25 </br>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
